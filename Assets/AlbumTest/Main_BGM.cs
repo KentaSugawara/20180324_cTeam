80bf005a188ef0be8c591c5a4ad9f60e4a0e12c6
KentaSugawara <KentaSugawara1031@gmail.com>
2018-06-16 18:40:07 +0900

    no message

---
+++ Assets/AlbumTest/Main_BGM.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_BGM : MonoBehaviour {
+    IEnumerator BGMRoutine;
+
+    private AudioSource _Audio_BGM;
+
+    private void Awake()
+    {
+        _Audio_BGM = GetComponent<AudioSource>();
+    }
+
+    public void BGM_In()
+    {
+        if (BGMRoutine != null) StopCoroutine(BGMRoutine);
+        BGMRoutine = Routine_BGM_In();
+        StartCoroutine(BGMRoutine);
+    }
+
+    public void BGM_Out()
+    {
+        if (BGMRoutine != null) StopCoroutine(BGMRoutine);
+        BGMRoutine = Routine_BGM_Out();
+        StartCoroutine(Routine_BGM_Out());
+    }
+
+    private IEnumerator Routine_BGM_In()
+    {
+        _Audio_BGM.Play();
+        for (float t = 0.0f; t < 2.0f; t += Time.deltaTime)
+        {
+            _Audio_BGM.volume = Mathf.Lerp(0.0f, 1.0f, t / 2.0f);
+            yield return null;
+        }
+    }
+
+    private IEnumerator Routine_BGM_Out()
+    {
+        for (float t = 0.0f; t < 2.0f; t += Time.deltaTime)
+        {
+            _Audio_BGM.volume = Mathf.Lerp(1.0f, 0.0f, t / 2.0f);
+            yield return null;
+        }
+        _Audio_BGM.Stop();
+    }
+}
---
+++ Assets/AlbumTest/Main_PictureBookNew.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_PictureBookNew : MonoBehaviour {
+    [SerializeField]
+    private RectTransform _New;
+
+    [SerializeField]
+    private float _OpenNeedSeconds;
+
+    [SerializeField]
+    private float _ViewSeconds;
+
+    [SerializeField]
+    private float _HideNeedSeconds;
+
+    IEnumerator RoutineNew;
+
+    public void HideNew()
+    {
+        if (RoutineNew != null) StopCoroutine(RoutineNew);
+        _New.localScale = Vector3.zero;
+        _New.gameObject.SetActive(false);
+    }
+
+    public void ViewNew()
+    {
+        if (RoutineNew != null) StopCoroutine(RoutineNew);
+        RoutineNew = Routine_New();
+        _New.gameObject.SetActive(true);
+        StartCoroutine(RoutineNew);
+    }
+
+    private IEnumerator Routine_New()
+    {
+        _New.localScale = Vector3.zero;
+        Vector3 b;
+        for (float t = 0.0f; t < _OpenNeedSeconds; t += Time.deltaTime)
+        {
+            float e = t / _OpenNeedSeconds;
+            b = Vector3.Lerp(Vector3.zero, Vector3.one, e);
+            _New.localScale = Vector3.Lerp(b, Vector3.one, e);
+            yield return null;
+        }
+        _New.localScale = Vector3.one;
+
+        yield return new WaitForSeconds(_ViewSeconds);
+
+        for (float t = 0.0f; t < _HideNeedSeconds; t += Time.deltaTime)
+        {
+            float e = t / _HideNeedSeconds;
+            b = Vector3.Lerp(Vector3.one, Vector3.zero, e);
+            _New.localScale = Vector3.Lerp(b, Vector3.zero, e);
+            yield return null;
+        }
+        _New.localScale = Vector3.zero;
+        _New.gameObject.SetActive(true);
+    }
+}
