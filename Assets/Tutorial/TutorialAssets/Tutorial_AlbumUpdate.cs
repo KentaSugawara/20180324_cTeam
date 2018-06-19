9903de50b466207462c87c0ae8e311d594561c05
KentaSugawara <KentaSugawara1031@gmail.com>
2018-06-19 16:04:16 +0900

    チュートリアル途中

---
+++ Assets/Tutorial/TutorialAssets/Tutorial_AlbumUpdate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_AlbumUpdate : TutorialMethod {
+    [SerializeField]
+    private int _CharaCloseIndex;
+
+    [SerializeField]
+    private float _WaitSeconds;
+
+    [SerializeField]
+    private float _ViewSeconds;
+
+    public void UpdateAlbum()
+    {
+        var s = new SnapShotInfo();
+        s.CharaCloseIndex = _CharaCloseIndex;
+        var list = new List<KeyValuePair<GameObject, SnapShotInfo>>() { new KeyValuePair<GameObject, SnapShotInfo>(null, s) };
+        Main_PictureBookManager.UpdateAlbum(list);
+        //Main_ChallengeManager.CheckChallenges(list);
+    }
+
+    public override void Method(Action endcallback)
+    {
+        StartCoroutine(Routine_Method(endcallback));
+    }
+
+    private IEnumerator Routine_Method(Action endcallback)
+    {
+        yield return new WaitForSeconds(_WaitSeconds);
+        UpdateAlbum();
+        yield return new WaitForSeconds(_ViewSeconds);
+        endcallback();
+    }
+}
