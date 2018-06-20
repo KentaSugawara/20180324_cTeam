ca9c0e260c7dc0a70891fb6cc8a360167ab12e5f
KentaSugawara <KentaSugawara1031@gmail.com>
2018-06-20 22:00:25 +0900

    アイテム設置UIの改善 アルバムのソートを降順に ゴミ箱のinfoに枚数を追加

--- Assets/AlbumTest/Item/Main_ItemDragObj.cs
+++ Assets/AlbumTest/Item/Main_ItemDragObj.cs
@@ -6,6 +6,13 @@ using UnityEngine.UI;
 public class Main_ItemDragObj : MonoBehaviour {
     [SerializeField]
     private Image _Image;
+    public Image ItemImage
+    {
+        get { return _Image; }
+    }
+
+    [SerializeField]
+    private GameObject _Obj_CanNotSet;
 
     private int _ItemIndex;
 
@@ -14,6 +21,19 @@ public class Main_ItemDragObj : MonoBehaviour {
         _Image.sprite = sprite;
         _ItemIndex = ItemIndex;
         transform.position = Input.mousePosition;
+    }
+
+    public void SetActive_CanNotSetImage(bool value)
+    {
+        var color = _Image.color;
+        if (value)
+        {
+            _Image.color = Color.black;
+        }
+        else
+        {
+            _Image.color = Color.white;
+        }
     }
 
     private void Update()
